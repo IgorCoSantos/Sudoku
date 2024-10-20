677f6a3fb051593d1ea36fc49221e84035dab765
Igor Coelho Santos <igor.coelhosantos@outlook.com>
2024-10-20 10:39:54 -0300

    att def exibe_matriz - legenda linhas
    add legenda das linhas no def exibe_matriz

--- codigos/recebe_sudoku.py
+++ codigos/recebe_sudoku.py
@@ -1,12 +1,29 @@
 def exibe_matriz(matriz):
     """
+    Obj - exibir a matriz na forma expandida
+
+    parâmetros
+    matriz - matriz a ser exibida
+
+    retorno - print da matriz em sua forma expandida
     """
     n = len(matriz[0])
     m = len(matriz)
+
     for i in range (0,n):
         print()
+        
         for j in range (0,m):
-            print(f'{matriz[i][j]}', end = ' ')
+
+            if j == 0:
+                print(f'{i+1}', end = '  |  ')
+
+            if j == m-1:
+                print(f'{matriz[i][j]}', end = '  |')
+            else:
+                print(f'{matriz[i][j]}', end = '  ')
+
+
 
 def cria_matriz(n = 9, m = 9):
     """
@@ -21,13 +38,18 @@ def cria_matriz(n = 9, m = 9):
 
     matriz = []
     vetor = []
+
     for i in range (0,n):
+
         for j in range (0,m):
             vetor.append(((i + 1) * 10) + (j + 1))
+
         matriz.append(vetor[:])
         vetor.clear()
     
     return matriz
+
+
 
 
 matriz = cria_matriz()
